9a174b3d86520ff01e4004235b44cfecfedf04fb
tfreville <tfreville@tankyou.co>
2021-03-31 22:46:49 +0200

    feat: Add base script for package

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# pymock
+# elmock
+
 A simple python package to help dynamic mock class creation.
+This package is inspired by golang testify/mock tool.
---
+++ src/elmock/__init__.py
@@ -0,0 +1,6 @@
+"""
+Pymock package propose a base class using Builder design pattern
+to create mocked class for testing.
+"""
+
+from .mocker import Mock
